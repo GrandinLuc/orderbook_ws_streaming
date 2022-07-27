ac0cc93e6e076690e40fd67817dd1cb8c3e9b11e
luc <luc.grandin@edu.devinci.fr>
2022-07-27 17:53:23 +0200

    Made the connection to Binance and Bitstamp websocket APIs and deserialize the data in a usable format

--- src/binance.rs
+++ src/binance.rs
@@ -1,14 +1,40 @@
-use serde_json;
-use tungstenite::{connect, Message};
+use serde_derive::{Deserialize, Serialize};
+use tungstenite::connect;
 use url::Url;
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+struct ApiResult {
+    last_update_id: u64,
+    bids: Vec<[String; 2]>,
+    asks: Vec<[String; 2]>,
+}
+
 fn main() {
-    let (mut socket, response) =
+    let (mut socket, _response) =
         connect(Url::parse("wss://stream.binance.com:9443/ws/ethbtc@depth20@100ms").unwrap())
             .expect("Can't connect");
 
     loop {
         let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
+        let msg = match msg {
+            tungstenite::Message::Text(s) => s,
+            _ => String::from(""),
+        };
+
+        if msg != String::from("") {
+            let parsed: Result<ApiResult, serde_json::Error> = serde_json::from_str(&msg);
+
+            match parsed {
+                Ok(parsed) => {
+                    println!("Received: {:?}", &parsed.bids[0..10]);
+                    println!("Received: {:?}", &parsed.last_update_id)
+                }
+                Err(parsed) => {
+                    panic!("Error: {:?}", parsed);
+                }
+            }
+        }
     }
 }
--- src/bitstamp.rs
+++ src/bitstamp.rs
@@ -1,9 +1,25 @@
+use serde_derive::{Deserialize, Serialize};
 use serde_json::json;
 use tungstenite::{connect, Message};
 use url::Url;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    timestamp: String,
+    microtimestamp: String,
+    bids: Vec<[String; 2]>,
+    asks: Vec<[String; 2]>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiResult {
+    data: Data,
+    channel: String,
+    event: String,
+}
+
 fn main() {
-    let (mut socket, response) =
+    let (mut socket, _response) =
         connect(Url::parse("wss://ws.bitstamp.net").unwrap()).expect("Can't connect");
 
     let request = json!({
@@ -20,6 +36,22 @@ fn main() {
     loop {
         let msg = socket.read_message().expect("Error reading message");
 
-        println!("Received: {}", msg);
+        let msg = match msg {
+            tungstenite::Message::Text(s) => s,
+            _ => String::from(""),
+        };
+
+        if msg != String::from("") {
+            let parsed: Result<ApiResult, serde_json::Error> = serde_json::from_str(&msg);
+
+            match parsed {
+                Ok(parsed) => {
+                    println!("Received: {:?}", &parsed.data.bids[0..10]);
+                }
+                Err(parsed) => {
+                    panic!("Error: {:?}", parsed);
+                }
+            }
+        }
     }
 }
